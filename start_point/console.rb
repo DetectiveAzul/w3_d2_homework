9850675e6c56e16827369e1e48d92704d0729a05
Jaime Lopez <detec.azul@gmail.com>
2018-03-20 16:09:24 +0000

    MVP completed

---
+++ start_point/console.rb
@@ -0,0 +1,63 @@
+require_relative('./models/bounty')
+
+bounty01_hash = {
+  'name' => 'Han Solo',
+  'species' => 'Human',
+  'bounty_value' => '30000',
+  'danger_level' =>'medium',
+  'last_known_location' =>'Tatooine',
+  'homeworld' =>'Coruscant',
+  'favorite_weapon' =>'Hand Blaster',
+  'cashed_in' => 'yes',
+  'collected_by' => 'Bobba Fett'
+}
+bounty02_hash = {
+  'name' => 'Chewbacca',
+  'species' => 'Wookie',
+  'bounty_value' => '50000',
+  'danger_level' => 'high',
+  'last_known_location' => 'Tatooine',
+  'homeworld' => 'Kashyyk',
+  'favorite_weapon' => 'Wookie Crossbow',
+  'cashed_in' => 'no'
+}
+bounty03_hash = {
+  'name' =>'Bulduga',
+  'bounty_value' => 25000,
+  'danger_level' => 'low',
+  'favorite_weapon' => 'Vibroknife',
+  'cashed_in' => 'no'
+}
+bounty04_hash = {
+  'name' =>'Greedo',
+  'species' =>'Rodian',
+  'bounty_value' =>20000,
+  'danger_level' =>'high',
+  'homeworld' =>'Rodian-2',
+  'cashed_in' =>'yes',
+  'collected_by' =>'Han Solo'
+}
+
+bounty01 = Bounty.new(bounty01_hash)
+bounty02 = Bounty.new(bounty02_hash)
+bounty03 = Bounty.new(bounty03_hash)
+bounty04 = Bounty.new(bounty04_hash)
+
+# Bounty.delete_table()
+# Bounty.create_table()
+
+# bounty01.save()
+# bounty02.save()
+# bounty03.save()
+# bounty04.save()
+
+all_bounties_object = Bounty.all()
+# my_bounty = all_bounties_object.last
+# my_bounty.cashed_in = 'yes'
+# my_bounty.collected_by = 'Pawel'
+# my_bounty.update()
+
+deleting_bounty = all_bounties_object.first
+deleting_bounty.delete()
+
+# Bounty.delete_all()
---
+++ start_point/models/Bounty.rb
@@ -0,0 +1,105 @@
+require('pg')
+class Bounty
+  attr_accessor :id, :name, :species, :bounty_value,
+              :danger_level, :last_known_location,
+              :homeworld, :favorite_weapon, :cashed_in,
+              :collected_by
+  def initialize(information)
+    @id = information['id'].to_i
+    @name = information['name']
+    @species = information['species']
+    @bounty_value = information['bounty_value'].to_i
+    @danger_level = information['danger_level']
+    @last_known_location = information['last_known_location']
+    @homeworld = information['homeworld']
+    @favorite_weapon = information['favorite_weapon']
+    @cashed_in = information['cashed_in']
+    @collected_by = information['collected_by']
+  end
+
+  def Bounty.delete_table()
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost' } )
+    a_sql = "DROP TABLE bounties;"
+    db.prepare("delete_table", a_sql)
+    db.exec_prepared("delete_table")
+    db.close()
+  end
+
+  def Bounty.create_table()
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost' } )
+    a_sql = "CREATE TABLE bounties (
+      id SERIAL8 PRIMARY KEY,
+      name VARCHAR(255),
+      species VARCHAR(255),
+      bounty_value INT,
+      danger_level VARCHAR(255),
+      last_known_location VARCHAR(255),
+      homeworld VARCHAR(255),
+      favorite_weapon VARCHAR(255),
+      cashed_in VARCHAR(255),
+      collected_by VARCHAR(255)
+    );"
+    db.prepare("create_table", a_sql)
+    db.exec_prepared("create_table")
+    db.close()
+  end
+
+  def Bounty.all()
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost' } )
+    a_sql = "SELECT * FROM bounties;"
+    db.prepare("select_all", a_sql)
+    all_bounties = db.exec_prepared("select_all")
+    db.close()
+    return all_bounties.map { |bounty_hash| Bounty.new(bounty_hash) }
+  end
+
+  def Bounty.delete_all()
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost' } )
+    a_sql = "DELETE FROM bounties;"
+    db.prepare("delete_all", a_sql)
+    db.exec_prepared("delete_all")
+    db.close()
+  end
+
+  def save()
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost' } )
+    a_sql = "INSERT INTO bounties
+    (name, species, bounty_value, danger_level, last_known_location,
+    homeworld, favorite_weapon, cashed_in, collected_by)
+    VALUES
+    ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+    ;"
+    values = [@name, @species, @bounty_value, @danger_level, @last_known_location,
+    @homeworld, @favorite_weapon, @cashed_in, @collected_by]
+    db.prepare("save_it",a_sql)
+    db.exec_prepared("save_it",values)
+    db.close()
+  end
+
+  def update()
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost' } )
+    a_sql = "UPDATE bounties
+      SET (name, species, bounty_value, danger_level, last_known_location,
+      homeworld,favorite_weapon,cashed_in,collected_by)
+      = ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      WHERE id = $10
+    ;"
+    values = [@name, @species, @bounty_value, @danger_level, @last_known_location,
+       @homeworld, @favorite_weapon, @cashed_in, @collected_by, @id]
+    db.prepare("update_it",a_sql)
+    db.exec_prepared("update_it",values)
+    db.close()
+  end
+
+  def delete()
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost' } )
+    a_sql = "
+      DELETE FROM bounties
+      WHERE id = $1
+    ;"
+    values = [@id]
+    db.prepare("delete_it", a_sql)
+    db.exec_prepared("delete_it", values)
+    db.close()
+  end
+end
